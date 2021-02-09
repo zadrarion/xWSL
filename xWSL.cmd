d7726f23e02130ed0c4d35d986309ea000dc107d
zadrarion <zadrarion.game@gmail.com>
2021-02-09 09:47:47 +0100

    Update xWSL.cmd

--- xWSL.cmd
+++ xWSL.cmd
@@ -1,7 +1,7 @@
 @ECHO OFF & NET SESSION >NUL 2>&1 
 IF %ERRORLEVEL% == 0 (ECHO Administrator check passed...) ELSE (ECHO You need to run this command with administrative rights.  Is User Account Control enabled? && pause && goto ENDSCRIPT)
 COLOR 1F
-SET GITORG=DesktopECHO
+SET GITORG=zadrarion
 SET GITPRJ=xWSL
 SET BRANCH=master
 SET BASE=https://github.com/%GITORG%/%GITPRJ%/raw/%BRANCH%
